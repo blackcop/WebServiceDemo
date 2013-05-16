284cc552b72a817a7ec6bf3e21e35e12312f27ae
peifei <blackcop@foxmail.com>
2013-05-16 13:57:58 +0800

    加入接口实现类

--- src/main/java/org/example/ws/pojo/CouponInfoOfAccount.java
+++ src/main/java/org/example/ws/pojo/CouponInfoOfAccount.java
@@ -1,5 +1,8 @@
 package org.example.ws.pojo;
 
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "CouponInfoOfAccount")
 public class CouponInfoOfAccount {
 
 	private String commercialName;

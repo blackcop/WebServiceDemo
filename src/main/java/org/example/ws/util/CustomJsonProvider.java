91beac1fdd66845f4c96fd92a85bd6543438fb24
peifei <blackcop@foxmail.com>
2013-06-04 12:03:06 +0800

    修复没根节点的情况

--- src/main/java/org/example/ws/util/CustomJsonProvider.java
+++ src/main/java/org/example/ws/util/CustomJsonProvider.java
@@ -1,15 +1,21 @@
 package org.example.ws.util;
 
 import org.codehaus.jackson.jaxrs.JacksonJaxbJsonProvider;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.xc.JaxbAnnotationIntrospector;
 
 public class CustomJsonProvider extends JacksonJaxbJsonProvider {
 
     public CustomJsonProvider(){
     	//可以定制jackson处理json的方式
-//        ObjectMapper mapper = new ObjectMapper();
-//        mapper.configure(org.codehaus.jackson.map.DeserializationConfig.Feature.UNWRAP_ROOT_VALUE, true);
-//        mapper.configure(org.codehaus.jackson.map.SerializationConfig.Feature.WRAP_ROOT_VALUE, true);
-//        _mapperConfig.setMapper(mapper);
-//        _mapperConfig.getConfiguredMapper().setAnnotationIntrospector(new JaxbAnnotationIntrospector());
+		ObjectMapper mapper = new ObjectMapper();
+		mapper.configure(
+				org.codehaus.jackson.map.DeserializationConfig.Feature.UNWRAP_ROOT_VALUE,
+				true);
+		mapper.configure(
+				org.codehaus.jackson.map.SerializationConfig.Feature.WRAP_ROOT_VALUE,
+				true);
+		_mapperConfig.setMapper(mapper);
+		_mapperConfig.getConfiguredMapper().setAnnotationIntrospector(new JaxbAnnotationIntrospector());
     }
 }
